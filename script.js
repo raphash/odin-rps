7f743692a9a0611c07a416de720b1fb182bad001
raphash <175216103+raphash@users.noreply.github.com>
2024-11-07 12:11:11 -0300

    feat: Function to get human choice

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
 
-  switch (choice) {
-    case 0:
-      return "rock";
-      break;
-    case 1:
-      return "paper";
-      break;
-    case 2:
-      return "scissor";
-      break;
-  }
+  return ["rock", "paper", "scissor"][choice];
 }
 
+function getHumanChoice() {
+  // Allowed choices
+  let userChoice;
+  let allowedChoices = {
+    "rock": "rock",
+    "paper": "paper",
+    "scissor": "scissor"
+  };
+
+  // While the user choice is not in allowedChoices: prompt user.
+  while (!(allowedChoices[userChoice]) && (userChoice !== null)) {
+    userChoice = prompt("Choice: ");
+
+    // If choice is not undefined or null convert it to lowercase.
+    if (userChoice != null) {
+      userChoice = userChoice.toLowerCase();  
+    }
+  }
+
+  // If the user choice matches and return then.
+  if (allowedChoices[userChoice]) {
+    return allowedChoices[userChoice];
+  } 
+
+  return "Good bye!";
+}
+
+console.log(getHumanChoice())
