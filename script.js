6688d214432007cf4307f1f79f52989428276ac4
raphash <175216103+raphash@users.noreply.github.com>
2024-11-07 15:28:44 -0300

    feat: Function to play one round

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
 
@@ -15,20 +18,46 @@ function getHumanChoice() {
 
   // While the user choice is not in allowedChoices: prompt user.
   while (!(allowedChoices[userChoice]) && (userChoice !== null)) {
-    userChoice = prompt("Choice: ");
+    userChoice = prompt("Rock, paper or scissor?");
 
     // If choice is not undefined or null convert it to lowercase.
     if (userChoice != null) {
-      userChoice = userChoice.toLowerCase();  
+      userChoice = userChoice.toLowerCase();
     }
   }
 
   // If the user choice matches and return then.
   if (allowedChoices[userChoice]) {
     return allowedChoices[userChoice];
-  } 
-
-  return "Good bye!";
+  }
 }
 
-console.log(getHumanChoice())
+function playRound(computerChoice, humanChoice) {
+  let possibilities = {
+    "rock": "scissor",
+    "paper": "rock",
+    "scissor": "paper"
+  }
+
+  for (let possibilty in possibilities) {
+    // Check if computer wins.
+    if (computerChoice === possibilty && humanChoice === possibilities[possibilty]) {
+      console.log(`You lose! ${possibilty} wins ${possibilities[possibilty]}!`);
+      computerScore++;
+      break;
+    }
+
+    // Check if human wins.
+    if (humanChoice === possibilty && computerChoice === possibilities[possibilty]) {
+      console.log(`You win! ${possibilty} wins ${possibilities[possibilty]}!`);
+      humanScore++;
+      break;
+    }
+
+    if (humanChoice === computerChoice) {
+      console.log(`Tie! ${humanChoice} vs ${computerChoice}.`);
+      break;
+    }
+  }
+}
+
