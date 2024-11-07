69dadd225490f451c382e6dae3fb2400b42a18ba
raphash <175216103+raphash@users.noreply.github.com>
2024-11-07 15:58:35 -0300

    feat: Function to play a complete duel

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let computerScore = 0;
 let humanScore = 0;
+let roundWinner = "First Round"
 
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
@@ -18,7 +19,7 @@ function getHumanChoice() {
 
   // While the user choice is not in allowedChoices: prompt user.
   while (!(allowedChoices[userChoice]) && (userChoice !== null)) {
-    userChoice = prompt("Rock, paper or scissor?");
+    userChoice = prompt(`Rock, paper or scissor? | ${humanScore} x ${computerScore} | Winner: ${roundWinner}`);
 
     // If choice is not undefined or null convert it to lowercase.
     if (userChoice != null) {
@@ -39,10 +40,12 @@ function playRound(computerChoice, humanChoice) {
     "scissor": "paper"
   }
 
+  // Loop through the possibilities
   for (let possibilty in possibilities) {
     // Check if computer wins.
     if (computerChoice === possibilty && humanChoice === possibilities[possibilty]) {
       console.log(`You lose! ${possibilty} wins ${possibilities[possibilty]}!`);
+      roundWinner = "Computer";
       computerScore++;
       break;
     }
@@ -50,14 +53,38 @@ function playRound(computerChoice, humanChoice) {
     // Check if human wins.
     if (humanChoice === possibilty && computerChoice === possibilities[possibilty]) {
       console.log(`You win! ${possibilty} wins ${possibilities[possibilty]}!`);
+      roundWinner = "You";
       humanScore++;
       break;
     }
 
+    // With both choose the same is tie.
     if (humanChoice === computerChoice) {
       console.log(`Tie! ${humanChoice} vs ${computerChoice}.`);
+      roundWinner = "Tie!";
       break;
     }
   }
 }
 
+function playGame() {
+  
+  // Runs until the score is five.
+  while (computerScore !== 5 || humanScore !== 5) {
+    playRound(getComputerChoice(), getHumanChoice());
+
+    // Show computer wins
+    if (computerScore === 5) {
+      alert(`Computer wins! ${computerScore} x ${humanScore}`);
+      break;
+    }
+    
+    // Show human wins
+    if (humanScore === 5) {
+      alert(`You win! ${humanScore} x ${computerScore}`);
+      break;
+    }
+  }
+}
+
+playGame();
