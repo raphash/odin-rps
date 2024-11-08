61fe06ad625de363ae1565834507d2d5be3f9a32
raphash <175216103+raphash@users.noreply.github.com>
2024-11-08 09:17:41 -0300

    feat: Function to return human choice
    This function returns the user's choice, with the allowed
    options being "Rock", "Paper" or "Scissors". Otherwise, it
    will prompt again, but if the user wishes to leave, they are
    completely free to do so.

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 // PSEUDOCODE:
 // 1: Generate a random number.
 // 2: Using the generated number return "Rock", "Paper" or "Scissors".
+// 3: Get the human input
+// 4: Convert it to lowercase
+// 5: Return only "rock", "paper" or "scissors" else ask again
+
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
 
   switch (choice) {
-    case 0: return "Rock";
-    case 1: return "Paper";
-    case 2: return "Scissors";
+    case 0: return "rock";
+    case 1: return "paper";
+    case 2: return "scissors";
   }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+  let choice = prompt("Rock, paper or scissors?");
+
+  // This ends the game if user cancel.
+  if (!choice) return "Good bye!";
+
+  while (true) {
+    // Choice is true? Convert to lowercase.
+    switch (choice && choice.toLowerCase()) {
+      case "rock": return "rock";
+      case "paper": return "paper";
+      case "scissors": return "scissors";
+
+      default:
+        choice = prompt("Rock, paper or scissors?");
+    }
+  }
+}
