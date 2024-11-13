cf3e908bf9969247a27fcfbdf5dbde826caa281d
raphash <175216103+raphash@users.noreply.github.com>
2024-11-13 17:05:06 -0300

    feat: Choice button logic & working rounds list
    This feature adds player choice buttons
    logic and inserts in a list of rounds the
    current round result.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const choices = document.querySelector(".choices");
+const results = document.querySelector(".results");
+
 function getComputerChoice() {
   const computerChoice = Math.floor(Math.random() * 3);
 
@@ -8,21 +11,18 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  let humanChoice = prompt("Rock, paper or scissors?");
-
-  if (!humanChoice) return;
-
-  while (true) {
-    switch (humanChoice.toLowerCase()) {
-      case "rock": return "rock";
-      case "paper": return "paper";
-      case "scissors": return "scissors";
-
-      default:
-        humanChoice = prompt("Rock, paper or scissors?");
-    }
+function getHumanChoice(event) {
+  switch (event.target.className) {
+    case "rock": return "rock";
+    case "paper": return "paper";
+    case "scissors": return "scissors";
   }
+}
+
+function setRoundResult(string) {
+  const li = document.createElement("li");
+  li.textContent = string;
+  results.appendChild(li);
 }
 
 function playGame() {
@@ -30,43 +30,56 @@ function playGame() {
   let computerChoice;
   let humanScore = 0;
   let computerScore = 0;
+  let currentRound = 0;
 
   function playRound(humanChoice, computerChoice) {
-    // Victory possibilities winner:loser.
-    const possibilities = `paper:rock rock:scissors scissors:paper`;
-    let isHumanWinner = possibilities.includes(`${humanChoice}:${computerChoice}`);
-    let isComputerWinner = possibilities.includes(`${computerChoice}:${humanChoice}`);
+    function checkRoundWinner() {
+      // Victory possibilities winner:loser.
+      const possibilities = `paper:rock rock:scissors scissors:paper`;
+      let isHumanWinner = possibilities.includes(`${humanChoice}:${computerChoice}`);
+      let isComputerWinner = possibilities.includes(`${computerChoice}:${humanChoice}`);
 
-    // Check the winner, the loser or draw and increment the score.
-    if (isHumanWinner) {
-      console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-      humanScore++;
-    } else if (isComputerWinner) {
-      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-      computerScore++;
-    } else {
-      console.log(`It's a draw! ${humanChoice} x ${computerChoice}.`);
+      // Check the winner, the loser or draw and increment the score.
+      if (isHumanWinner) {
+        setRoundResult(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+      } else if (isComputerWinner) {
+        setRoundResult(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+      } else {
+        setRoundResult(`It's a draw! ${humanChoice} x ${computerChoice}.`);
+      }
     }
+
+    function resetGame() {
+      currentRound = 0;
+      humanScore = 0;
+      computerScore = 0;
+    }
+
+    function checkFinalWinner() {
+      if (currentRound == 5) {
+        if (humanScore > computerScore) {
+          console.log(`You win! ${humanScore} x ${computerScore}`);
+        } else if (computerScore > humanScore) {
+          console.log(`You lose! ${computerScore} x ${humanScore}`);
+        } else if (humanChoice != null) {
+          console.log(`It's a draw! ${humanScore} x ${computerScore}`);
+        } 
+
+        resetGame();
+      }
+    }
+
+    checkRoundWinner();
+    checkFinalWinner();
   }
 
-  // Start five rounds.
-  for (let i = 0; i < 5; i++) {
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-
-    if (humanChoice == null) break;
-    
-    playRound(humanChoice, computerChoice);
-  }
-
-  // This shows the five rounds final result.
-  if (humanScore > computerScore) {
-    console.log(`You win! ${humanScore} x ${computerScore}`);
-  } else if (computerScore > humanScore) {
-    console.log(`You lose! ${computerScore} x ${humanScore}`);
-  } else if (humanChoice != null) {
-    console.log(`It's a draw! ${humanScore} x ${computerScore}`);
-  } else {
-    console.log(`Good bye!`);
-  }
+  // This listener is resposible by player choice buttons.
+  choices.addEventListener("click", (event)=>{
+    currentRound++;
+    playRound(getHumanChoice(event), getComputerChoice());
+  });
 }
+
+playGame()
